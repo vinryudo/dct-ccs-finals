d6007e09e5bf87a8c2406cd74b17bd0a766da695
vinryudo <davidpaulnathaniel01@gmail.com>
2024-11-20 20:22:43 +0800

    added the links to the dashboard, subject addition, and student registration

--- admin/partials/side-bar.php
+++ admin/partials/side-bar.php
@@ -7,19 +7,19 @@
         <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
             <ul class="nav flex-column">
                 <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center gap-2" href="#">
+                    <a class="nav-link d-flex align-items-center gap-2" href="/admin/dashboard.php">
                         <i class="fa-solid fa-gauge fa-fw me-2"></i>
                         Dashboard
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center gap-2" href="#">
+                    <a class="nav-link d-flex align-items-center gap-2" href="/admin/subject/add.php">
                     <i class="fa-solid fa-book-open me-2 fa-fw"></i>
                         Subjects
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center gap-2" href="#">
+                    <a class="nav-link d-flex align-items-center gap-2" href="/admin/student/register.php">
                         <i class="fa-solid fa-user fa-fw me-2"></i>
                         Students
                     </a>
