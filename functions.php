db0435e72f4a6c97bd07f34aeb2d6a7c3f1247fe
vinryudo <davidpaulnathaniel01@gmail.com>
2024-11-20 18:25:49 +0800

    added the logout function

--- functions.php
+++ functions.php
@@ -88,4 +88,13 @@
             exit;
         }
     }
+
+    function logout_user() {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start(); // Start the session if not already started
+        }
+        session_destroy(); // Destroy the session
+        header("Location:../index.php"); // Redirect to root login page
+        exit();
+    }
 ?>
